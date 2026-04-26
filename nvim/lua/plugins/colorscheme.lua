ea2b716434ed2750cb2697de8c0c7dad1518e294
Christoph Zengler <christoph.zengler@booleworks.com>
2026-04-26 09:48:48 +0200

    Rose Pine Theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,28 +4,52 @@ return {
     'f-person/auto-dark-mode.nvim',
     opts = {},
   },
+
   {
-    'folke/tokyonight.nvim',
+    'rose-pine/neovim',
     lazy = false,
     priority = 1000,
+    name = 'rose-pine',
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        style = 'night',
-        light_style = 'day',
-        transparent = true,
+      require('rose-pine').setup {
+        variant = 'auto', -- auto, main, moon, or dawn
+        dark_variant = 'moon', -- main, moon, or dawn
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+
         styles = {
-          comments = { italic = true },
-          keywords = { italic = false },
-          sidebars = 'transparent',
-          floats = 'transparent',
+          bold = true,
+          italic = true,
+          transparency = false,
         },
-        -- sidebars = { 'qf', 'help' }, -- darker background on sidebar-like windows
-        -- day_brightness = 0.3,
-        dim_inactive = false,
-        lualine_bold = true,
       }
-      vim.cmd.colorscheme 'tokyonight-night'
+
+      vim.cmd 'colorscheme rose-pine'
     end,
   },
+
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     ---@diagnostic disable-next-line: missing-fields
+  --     require('tokyonight').setup {
+  --       style = 'night',
+  --       light_style = 'day',
+  --       transparent = true,
+  --       styles = {
+  --         comments = { italic = true },
+  --         keywords = { italic = false },
+  --         sidebars = 'transparent',
+  --         floats = 'transparent',
+  --       },
+  --       -- sidebars = { 'qf', 'help' }, -- darker background on sidebar-like windows
+  --       -- day_brightness = 0.3,
+  --       dim_inactive = false,
+  --       lualine_bold = true,
+  --     }
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --   end,
+  -- },
 }
